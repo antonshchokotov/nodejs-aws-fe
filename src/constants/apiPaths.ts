c5bdc74a43f61ca60db501f76c2cb157a80433ca
Anton Shchokotov <58229945+antonshchokotov@users.noreply.github.com>
2020-11-21 20:59:31 +0200

    fix: change apiPaths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 
 const API_PATHS = {
-  product: 'https://e049xelvjh.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://e049xelvjh.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://dkz6yf3yx9.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://e049xelvjh.execute-api.eu-west-1.amazonaws.com/dev'
+  product: 'https://m74ptczqwl.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://m74ptczqwl.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://ail80uyay7.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://m74ptczqwl.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
 export default API_PATHS;
